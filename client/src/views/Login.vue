157bee618cb5d952da85c9e041482b3280a922d0
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-04-21 13:06:55 +0200

    LocalstorageItem setter

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,23 +4,37 @@
       <h1 class="text-center my-3">Login</h1>
       <div class="row">
         <div class="col-3 offset-2">
-          <input class="form-control" type="email" v-model="email" placeholder="Email" required />
+          <input
+            class="form-control"
+            type="email"
+            v-model="email"
+            placeholder="Email"
+            required
+          />
         </div>
         <div class="col-3">
-          <input class="form-control" type="password" v-model="password" placeholder="Password" required />
+          <input
+            class="form-control"
+            type="password"
+            v-model="password"
+            placeholder="Password"
+            required
+          />
         </div>
         <div class="col-2">
           <button class="btn btn-primary">Login</button>
         </div>
       </div>
     </form>
-    <p class="mt-4 text-center">No account? <a href="/register">Register</a> first!</p>
+    <p class="mt-4 text-center">
+      No account? <a href="/register">Register</a> first!
+    </p>
     <p class="mt-auto text-center">&copy; 2021 Robert Baumgartner</p>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
   name: 'Login',
   data() {
@@ -31,7 +45,15 @@ export default {
   },
   methods: {
     async login() {
-      // enter your code here
+      let { data } = await axios({
+        url: 'http://localhost:3000/login',
+        method: 'POST',
+        data: {
+          email: this.email,
+          password: this.password,
+        },
+      });
+      localStorage.setItem('session', JSON.stringify({ id: data.id, name: data.name }));
     },
   },
 };
