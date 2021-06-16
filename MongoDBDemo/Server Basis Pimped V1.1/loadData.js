96d1dae1f4abd7f41dc2f5010e1e52d1a1122a8f
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-06-16 10:27:15 +0200

    Data now from dogs.json

--- MongoDBDemo/Server Basis Pimped V1.1/loadData.js
+++ MongoDBDemo/Server Basis Pimped V1.1/loadData.js
@@ -1,19 +1,12 @@
 require('./db/connect');
-require("./model/dogs")
+require('./model/dogs');
 require('colors');
 const { Dog } = require('./model/schemas');
+const dogs = require('./dogs.json');
 
 (async () => {
   try {
-    const p1 = Dog.create({
-      name: 'Bello',
-      born: '2020-12-01',
-    });
-    const p2 = Dog.create({
-      name: 'Beisser',
-      born: '2020-12-02',
-    });
-    await Promise.all([p1, p2]);
+    await Dog.create(dogs);
     console.log('Data loaded');
   } catch (error) {
     console.error(`Error ====> ${error.message.red}`);
