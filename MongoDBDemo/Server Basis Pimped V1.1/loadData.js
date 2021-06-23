c2b99ebb85a9c52eada98f26a113666ae27deec2
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-06-23 12:05:14 +0200

    Extend MongoDB to Nr2

--- MongoDBDemo/Server Basis Pimped V1.1/loadData.js
+++ MongoDBDemo/Server Basis Pimped V1.1/loadData.js
@@ -2,10 +2,26 @@ require('./db/connect');
 require('./model/dogs');
 require('colors');
 const { Dog } = require('./model/schemas');
-const dogs = require('./dogs.json');
+//const dogs = require('./dogs.json');
 
 (async () => {
   try {
+    const dogs = [
+      {
+        born: '1999-10-16',
+        name: 'Cupcake',
+        favFoods: ['le', 'meat'],
+        sex: 'mehl',
+        image: 'images/1.jpg',
+      },
+      {
+        born: '2001-04-22',
+        name: 'Britney Ears',
+        favFoods: ['turnips', 'corn'],
+        sex: 'female',
+        image: 'images/2.jpg',
+      },
+    ];
     await Dog.create(dogs);
     console.log('Data loaded');
   } catch (error) {
--- MongoDBDemo/Server Basis Pimped V1.1/model/schemas.js
+++ MongoDBDemo/Server Basis Pimped V1.1/model/schemas.js
@@ -1,9 +1,19 @@
 const { Schema, model } = require('mongoose');
 
 const dog = new Schema({
-  name: String,
-  born: Date,
+  name: { type: String, default: 'wau wau', unique: true },
+  born: { type: Date, min: new Date('2000-01-01'), max: Date.now, required: true },
+  favFoods: [
+    {
+      type: String,
+      minLength: 3,
+      maxLength: 20,
+    },
+  ],
+  sex: { type: String, enum: ['male', 'female'] },
+  image: String,
 });
+
 const Dog = model('dog', dog);
 
 module.exports = { Dog };
